475adf5f46cd51e2e5f7d6c3f5fc268148152342
Christina Lane <clane83@yahoo.com>
2026-05-23 08:16:45 -0700

    firt commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+using BlazingPizza.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddSingleton<PizzaService>();
+
+var app = builder.Build();
+
+
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
