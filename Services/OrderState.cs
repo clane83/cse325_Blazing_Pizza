3304be781ae196e5dc6f53fd892b005a1d6ffc1b
Christina Lane <clane83@yahoo.com>
2026-05-23 11:14:31 -0700

    module 3

--- Services/OrderState.cs
+++ Services/OrderState.cs
@@ -6,7 +6,7 @@ public class OrderState
 {
     public bool ShowingConfigureDialog { get; private set; }
 
-    public Pizza? ConfiguringPizza { get; private set; }
+    public Pizza ConfiguringPizza { get; private set; }
     public Order Order { get; private set; } = new Order();
 
 
